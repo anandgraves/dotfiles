3ff5883f6bcb3d4c659e34e421c55552a5abaa97
Anand Graves <anandgraves@users.noreply.github.com>
2017-04-30 13:46:46 +0200

    Global npm packages for installation

---
+++ installation/npm-global-packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+npm install -g pageres-cli
+npm install -g git-standup
+npm install -g trash-cli
+npm install -g nativefier
+npm install -g svgo
+npm install -g npm-check
+npm install -g wifi-password
